8a401a6efa1070088c8741b0047454c20da6b766
Miller, Samuel <smil@lle.rochester.edu>
2023-12-18 08:32:03 -0500

    spelling fix in arithmetic_mean

--- src/averaging.jl
+++ src/averaging.jl
@@ -1,4 +1,4 @@
 
 @inline harmonic_mean(a, b) = (2a * b) / (a + b)
-@inline arithmatic_mean(a, b) = (a + b) / 2
+@inline arithmetic_mean(a, b) = (a + b) / 2
 @inline geometric_mean(a, b) = sqrt(a * b)
