1534f94ada9da9986df3fc513c2aeedfb8bb1ab5
Miller, Samuel <smil@lle.rochester.edu>
2024-02-23 14:20:03 -0500

    update the max_dt function to use the new grid API

--- src/max_dt.jl
+++ src/max_dt.jl
@@ -5,21 +5,21 @@ using CurvilinearGrids
 
 export max_dt
 
-function max_dt(solver, mesh::CurvilinearGrid2D)
+function max_dt(diffusivity, mesh::CurvilinearGrid2D)
   maxdt = Inf
-  @unpack ilo, ihi, jlo, jhi = mesh.domain_limits.cell
-  CI = CartesianIndices((ilo:ihi, jlo:jhi))
 
-  @inline for idx in CI
+  @inline for idx in mesh.iterators.cell.domain
     # convert from linear index to cartesian tuple and
     # then add 1/2 to get the centroid indices
     centroid_idx = idx.I .+ 0.5
     @unpack ξx, ξy, ηx, ηy = metrics(mesh, centroid_idx)
 
-    dξ = 1 / sqrt(ξx^2 + ξy^2)
-    dη = 1 / sqrt(ηx^2 + ηy^2)
+    dξ = 1 / sqrt(mesh.cell_center_metrics.ξ.x[idx]^2 + #
+                  mesh.cell_center_metrics.ξ.y[idx]^2)
+    dη = 1 / sqrt(mesh.cell_center_metrics.η.x[idx]^2 + #
+                  mesh.cell_center_metrics.η.y[idx]^2)
 
-    α = solver.α[idx]
+    α = diffusivity[idx]
 
     dt = min(
       (dξ^2) / α, # ξ direction
