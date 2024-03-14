fca97fb68c0eb13c574d2e7a0e27f29ba8da1669
Sam Miller <smil@lle.rochester.edu>
2024-03-14 12:01:22 -0400

    updating ImplicitSolver members to be backend-compliant (gpu vs cpu)

--- src/implicit/ImplictSolver.jl
+++ src/implicit/ImplictSolver.jl
@@ -9,6 +9,8 @@ using SparseArrays
 using StaticArrays
 using AlgebraicMultigrid
 using Krylov
+using KrylovPreconditioners
+using CUDA
 
 using KernelAbstractions
 using LinearSolve
@@ -20,17 +22,15 @@ using .Threads
 
 export ImplicitScheme, solve!, assemble_matrix!
 
-struct ImplicitScheme{N,LP,SM,V,ST,T,F,BC,CI1,CI2}
+struct ImplicitScheme{N,T,AA<:AbstractArray{T,N},LP,SM,V,ST,F,BC,CI1,CI2}
   prob::LP # linear problem (contains A, b, u0, solver...)
   A::SM # sparse matrix
   x::V # solution vector
   u0::V
   b::V # RHS vector
   solver::ST # linear solver, e.g. GMRES, CG, etc.
-  # J::Array{T,N} # cell-centered Jacobian
-  # metrics::Array{EM,N}
-  α::Array{T,N} # cell-centered diffusivity
-  source_term::Array{T,N} # cell-centered source term
+  α::AA # cell-centered diffusivity
+  source_term::AA # cell-centered source term
   mean_func::F
   bcs::BC
   nhalo::Int
@@ -67,12 +67,12 @@ function ImplicitScheme(
 
   A = init_A_matrix(mesh, backend)
 
-  b = zeros(T, len)
-  u0 = zeros(T, len) # initial guess for iterative solvers
-  x = zeros(T, len)
+  b = KernelAbstractions.zeros(backend, T, len)
+  u0 = KernelAbstractions.zeros(backend, T, len) # initial guess for iterative solvers
+  x = KernelAbstractions.zeros(backend, T, len)
 
-  diffusivity = zeros(T, celldims)
-  source_term = zeros(T, celldims)
+  diffusivity = KernelAbstractions.zeros(backend, T, celldims)
+  source_term = KernelAbstractions.zeros(backend, T, celldims)
 
   # prob = LinearProblem(A, b)
 
@@ -126,11 +126,27 @@ function init_A_matrix(mesh::CurvilinearGrid2D, ::CPU)
     ni + 1  => zeros(len - ni - 1), # (i+1, j+1)
   )
   #! format: on
+
+  # kv = (
+  #   -ni - 1 => zeros(len - ni - 1), # (i-1, j-1)
+  #   -ni => zeros(len - ni),     # (i  , j-1)
+  #   -ni + 1 => zeros(len - ni + 1), # (i+1, j-1)
+  #   -1 => zeros(len - 1),      # (i-1, j  )
+  #   0 => ones(len),           # (i  , j  )
+  #   1 => zeros(len - 1),      # (i+1, j  )
+  #   ni - 1 => zeros(len - ni + 1), # (i-1, j+1)
+  #   ni => zeros(len - ni),     # (i  , j+1)
+  #   ni + 1 => zeros(len - ni - 1), # (i+1, j+1)
+  # )
+
+  # I, J, V, mmax, nmax = SparseArrays.spdiagm_internal(kv...)
+  # B = sparsecsr(I, J, V, mmax, nmax)
+
   return A
 end
 
 function init_A_matrix(mesh::CurvilinearGrid2D, ::GPU)
-  return CuSparseMatrixCSR(init_A_matrix(mesh, CPU()))
+  return CUDA.CUSPARSE.CuSparseMatrixCSR(init_A_matrix(mesh, CPU()))
 end
 
 function init_A_matrix(mesh::CurvilinearGrid1D, ::CPU)
@@ -154,8 +170,12 @@ function solve!(
   # end
 
   # use the current solution as a guess for the next
-  for (grid_idx, mat_idx) in zip(scheme.halo_aware_indices, domain_LI)
-    scheme.u0[mat_idx] = u[grid_idx]
+  # for (grid_idx, mat_idx) in zip(scheme.halo_aware_indices, domain_LI)
+  #   scheme.u0[mat_idx] = u[grid_idx]
+  # end
+
+  @views begin
+    copyto!(scheme.u0[domain_LI], u[scheme.halo_aware_indices])
   end
 
   prob = LinearProblem(scheme.A, scheme.b; u0=scheme.u0)
@@ -164,9 +184,14 @@ function solve!(
   # @timeit "solve_step" sol = solve(prob; Pl=LU)
 
   # update the solution to u in-place
-  for (grid_idx, mat_idx) in zip(scheme.halo_aware_indices, domain_LI)
-    u[grid_idx] = sol.u[mat_idx]
+  # for (grid_idx, mat_idx) in zip(scheme.halo_aware_indices, domain_LI)
+  #   u[grid_idx] = sol.u[mat_idx]
+  # end
+
+  @views begin
+    copyto!(u[scheme.halo_aware_indices], sol.u[domain_LI])
   end
+
   # return sol.resid, sol.iters, true
 
   return nothing
