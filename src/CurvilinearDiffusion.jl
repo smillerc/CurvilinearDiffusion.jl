db3510bc93868b331b69fd78ea7bd33f0853dfbb
Miller, Samuel <smil@lle.rochester.edu>
2024-05-04 07:58:58 -0400

    update exports from the top-level module

--- src/CurvilinearDiffusion.jl
+++ src/CurvilinearDiffusion.jl
@@ -7,7 +7,10 @@ using KernelAbstractions
 include("implicit/ImplictSolver.jl")
 using .ImplicitSchemeType
 export ImplicitScheme, solve!, assemble_matrix!, initialize_coefficient_matrix
-export DirichletBC, NeumannBC
+export DirichletBC, NeumannBC, applybc!, applybcs!
+
+include("nonlinear_thermal_conduction.jl")
+export nonlinear_thermal_conduction_step!
 
 include("conductivity.jl")
 export update_conductivity!
