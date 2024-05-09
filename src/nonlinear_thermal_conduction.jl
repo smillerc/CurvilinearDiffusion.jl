0692bfc801fd934ffe25104bcbd645ceb03893ec
Miller, Samuel <smil@lle.rochester.edu>
2024-05-09 11:33:03 -0400

    re-enable density bc application

--- src/nonlinear_thermal_conduction.jl
+++ src/nonlinear_thermal_conduction.jl
@@ -2,7 +2,7 @@ using TimerOutputs
 
 function nonlinear_thermal_conduction_step!(scheme::ImplicitScheme, mesh, T, ρ, cₚ, κ, Δt)
   @timeit "applybc!" applybcs!(scheme.bcs, mesh, T)
-  # @timeit "applybc!" applybcs!(scheme.bcs, mesh, ρ)
+  @timeit "applybc!" applybcs!(scheme.bcs, mesh, ρ)
   @timeit "update_conductivity!" update_conductivity!(scheme, T, ρ, cₚ, κ)
   @timeit "solve!" solve!(scheme, mesh, T, Δt)
 end
