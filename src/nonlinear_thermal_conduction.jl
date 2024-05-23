357a83596050dfbd05d8948f66a7b34090186515
Miller, Samuel <smil@lle.rochester.edu>
2024-05-23 17:06:32 -0400

    add the option to subcycle in the nonlinear_thermal_conduction_step!

--- src/nonlinear_thermal_conduction.jl
+++ src/nonlinear_thermal_conduction.jl
@@ -1,11 +1,25 @@
 using TimerOutputs
 
 function nonlinear_thermal_conduction_step!(
-  scheme::ImplicitScheme, mesh, T, ρ, cₚ, κ, Δt; cutoff=true
+  scheme::ImplicitScheme,
+  mesh,
+  T,
+  ρ,
+  cₚ,
+  κ,
+  Δt,
+  nsubcycles=1;
+  cutoff=true,
+  show_convergence=true,
 )
-  @timeit "applybc!" applybcs!(scheme.bcs, mesh, T)
-  # @timeit "applybc!" applybcs!(scheme.bcs, mesh, ρ)
-  @timeit "update_conductivity!" update_conductivity!(scheme, T, ρ, cₚ, κ)
-  @timeit "check_diffusivity_validity" check_diffusivity_validity(scheme)
-  @timeit "solve!" solve!(scheme, mesh, T, Δt; cutoff=cutoff)
+  for n in 1:nsubcycles
+    dt = Δt / n
+    @timeit "applybc!" applybcs!(scheme.bcs, mesh, T)
+    @timeit "applybc!" applybcs!(scheme.bcs, mesh, ρ)
+    @timeit "update_conductivity!" update_conductivity!(scheme, T, ρ, cₚ, κ)
+    @timeit "check_diffusivity_validity" check_diffusivity_validity(scheme)
+    @timeit "solve!" solve!(
+      scheme, mesh, T, dt; cutoff=cutoff, show_convergence=show_convergence
+    )
+  end
 end
