a79b5ce10d05bf3fb87efb61130abda8643612e7
Miller, Samuel <smil@lle.rochester.edu>
2024-05-24 10:30:58 -0400

    pass kwargs to the solver in nonlinear_thermal_conduction_step! and add the option to apply density boundary conditions

--- src/nonlinear_thermal_conduction.jl
+++ src/nonlinear_thermal_conduction.jl
@@ -11,15 +11,21 @@ function nonlinear_thermal_conduction_step!(
   nsubcycles=1;
   cutoff=true,
   show_convergence=true,
+  apply_density_bc=true,
+  kwargs...,
 )
   for n in 1:nsubcycles
     dt = Δt / n
     @timeit "applybc!" applybcs!(scheme.bcs, mesh, T)
-    @timeit "applybc!" applybcs!(scheme.bcs, mesh, ρ)
+
+    if apply_density_bc
+      @timeit "applybc!" applybcs!(scheme.bcs, mesh, ρ)
+    end
+
     @timeit "update_conductivity!" update_conductivity!(scheme, T, ρ, cₚ, κ)
     @timeit "check_diffusivity_validity" check_diffusivity_validity(scheme)
     @timeit "solve!" solve!(
-      scheme, mesh, T, dt; cutoff=cutoff, show_convergence=show_convergence
+      scheme, mesh, T, dt; cutoff=cutoff, show_convergence=show_convergence, kwargs...
     )
   end
 end
