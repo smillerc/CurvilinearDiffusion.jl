c149a38a7b6f4a44522eac478d40f41c5701f80f
Miller, Samuel <smil@lle.rochester.edu>
2024-06-14 16:27:10 -0400

    enforce_positivity errors when 0 is found now

--- src/validity_checks.jl
+++ src/validity_checks.jl
@@ -5,7 +5,7 @@ function validate_scalar(
   α_domain = @view α[domain]
 
   if enforce_positivity
-    domain_valid = (all(isfinite.(α_domain)) && all(map(x -> x >= 0, α_domain)))
+    domain_valid = (all(isfinite.(α_domain)) && all(map(x -> x > 0, α_domain)))
   else
     domain_valid = (all(isfinite.(α_domain)))
   end
@@ -23,8 +23,8 @@ function validate_scalar(
     α_hi = @view α[hi_edge]
 
     if enforce_positivity
-      α_lo_valid = (all(isfinite.(α_lo)) && all(map(x -> x >= 0, α_lo)))
-      α_hi_valid = (all(isfinite.(α_hi)) && all(map(x -> x >= 0, α_hi)))
+      α_lo_valid = (all(isfinite.(α_lo)) && all(map(x -> x > 0, α_lo)))
+      α_hi_valid = (all(isfinite.(α_hi)) && all(map(x -> x > 0, α_hi)))
     else
       α_lo_valid = all(isfinite.(α_lo))
       α_hi_valid = all(isfinite.(α_hi))
