9a7ad8e911dc3ec64b4371232be8f65bb0993c76
Miller, Samuel <smil@lle.rochester.edu>
2024-06-26 09:26:00 -0400

    first commit of a non-working pseudo-transient solver

---
+++ src/pseudo_transient/PseudoTransientSolver.jl
@@ -0,0 +1,117 @@
+module PseudoTransientScheme
+
+using CartesianDomains
+using CurvilinearGrids
+
+struct PseudoTransientSolver{N,T,AA<:AbstractArray{T,N},NT1}
+  H::AA
+  H_old::AA
+  qH::NT1
+  qH²::NT1
+  res::AA
+  Re::AA
+  α::AA
+  θr_dτ::AA
+  dτ_ρ::AA
+end
+
+function PseudoTransientSolver()
+  #
+  #         H
+  #     |--------|--------|--------|--------|
+  #           q_i+1/2
+  #
+
+  # cell-based
+  H = zeros(ni)
+  H_old = zeros(ni)
+
+  # edge-based
+  qH = (x = zeros(ni))
+  qH² = (x = zeros(ni))
+  res = zeros(ni)
+  Re = zeros(ni)
+  α = zeros(ni)
+  θr_dτ = zeros(ni)
+  dτ_ρ = zeros(ni)
+
+  return PseudoTransientSolver(H, H_old, qH, qH², res, Re, α, θr_dτ, dτ_ρ)
+end
+
+function solve!(solver, mesh, u, dt)
+  iter = 0
+  err = 2 * tol
+
+  # Pseudo-transient iteration
+  while err > tol && iter < itMax
+    # Diffusion coefficient
+    # D .= H .^ 3
+    update_conductivity!(solver, mesh, T, ρ, cₚ, κ)
+
+    # Assign iter params
+    @. Re = π + sqrt(π^2 + lx^2 / max(D, ε) / dt)
+
+    # θr_dτ -> θr / dτ
+    θr_dτ .= lx ./ Vpdτ ./ av(Re)
+
+    # dτ_ρ -> dτ /ρ
+
+    dτ_ρ .= Vpdτ .* lx ./ max.(D[2:(end - 1)], ε) ./ Re[2:(end - 1)]
+
+    # PT updates
+    # if implicit
+    #   qHx .= (qHx .* θr_dτ .- av(D) .* diff(H) ./ dx) ./ (1.0 .+ θr_dτ)
+    #   H[2:(end - 1)] .=
+    #     (H[2:(end - 1)] .+ dτ_ρ .* (Hold[2:(end - 1)] ./ dt .- diff(qHx) ./ dx)) ./
+    #     (1.0 .+ dτ_ρ ./ dt)
+    # else
+    @. qHx = qHx + 1 / θr_dτ * (-qHx - av(D) * diff(H) / dx)
+
+    H[2:(end - 1)] .=
+      H[2:(end - 1)] .+
+      dτ_ρ .* (.-(H[2:(end - 1)] .- Hold[2:(end - 1)]) ./ dt .- diff(qHx) ./ dx)
+    # end
+
+    iter += 1
+    # Check error (explicit residual)
+    if iter % nout == 0
+      qHx2 .= .-av(D) .* diff(H) ./ dx
+      ResH .= .-(H[2:(end - 1)] .- Hold[2:(end - 1)]) ./ dt .- diff(qHx2) ./ dx
+      err = norm(ResH) / sqrt(length(ResH))
+    end
+  end
+
+  # Update H
+  Hold .= H
+  ittot += iter
+  it += 1
+  t += dt
+
+  return nothing
+end
+
+function update_iteration_params!(θr_dτ, dτ_ρ, Vpdτ)
+  # θr_dτ -> θr / dτ
+  θr_dτ .= lx ./ Vpdτ ./ av(Re)
+
+  # dτ_ρ -> dτ /ρ
+
+  dτ_ρ .= Vpdτ .* lx ./ max.(D[2:(end - 1)], ε) ./ Re[2:(end - 1)]
+end
+
+function update_flux!(Hᵏ, Hᵏ⁻¹)
+  # fluxes are on the edge, H_i+1/2
+  for idx in domain
+    ᵢ₊₁ = shift(idx, +1, 1)
+    αᵢ₊½ = 0.5(α[idx] + α[ᵢ₊₁])
+    qHx = qHx + 1 / θr_dτ * (-qHx - av(D) * diff(H) / dx)
+
+    dτ_ρᵢ = dτ_ρ[idx]
+
+    Hᵏ[idx] = (Hᵏ[idx] + dτ_ρᵢ * (Hᵏ⁻¹[idx] / dt - ∇q)) / (1.0 + dτ_ρᵢ / dt)
+  end
+end
+
+function check_residual() end
+
+end # module
