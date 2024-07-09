f1b689636a2155c4d971968898a4596904af5de7
Miller, Samuel <smil@lle.rochester.edu>
2024-07-09 16:36:06 -0400

    add 3d for the PT solver scheme

--- src/pseudo_transient/PseudoTransientSolver.jl
+++ src/pseudo_transient/PseudoTransientSolver.jl
@@ -4,7 +4,7 @@ using LinearAlgebra: norm
 
 using TimerOutputs: @timeit
 using CartesianDomains: expand, shift, expand_lower, haloedge_regions
-using CurvilinearGrids: CurvilinearGrid2D, cellsize_withhalo, coords
+using CurvilinearGrids: CurvilinearGrid2D, CurvilinearGrid3D, cellsize_withhalo, coords
 using KernelAbstractions
 using KernelAbstractions: CPU, GPU, @kernel, @index
 using Polyester: @batch
@@ -79,6 +79,81 @@ function PseudoTransientSolver(
   min_x, max_x = extrema(x)
   min_y, max_y = extrema(y)
   L = max(abs(max_x - min_x), abs(max_y - min_y)) |> T
+
+  if face_diffusivity === :harmonic
+    mean_func = harmonic_mean # from ../averaging.jl
+  else
+    mean_func = arithmetic_mean # from ../averaging.jl
+  end
+
+  return PseudoTransientSolver(
+    u,
+    u_prev,
+    S,
+    q,
+    q′,
+    residual,
+    Reynolds_number,
+    α,
+    θr_dτ,
+    dτ_ρ,
+    spacing,
+    L,
+    iterators,
+    bcs,
+    mean_func,
+    backend,
+  )
+end
+
+function PseudoTransientSolver(
+  mesh::CurvilinearGrid3D, bcs; backend=CPU(), face_diffusivity=:arithmetic, T=Float64
+)
+  #
+  #         u
+  #     |--------|--------|--------|--------|
+  #           q_i+1/2
+  #
+
+  iterators = (
+    domain=(cartesian=mesh.iterators.cell.domain,),
+    full=(cartesian=mesh.iterators.cell.full,),
+  )
+
+  # cell-based
+  u = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+  u_prev = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+  S = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)) # source term
+
+  # edge-based
+  q = (
+    x=KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)),
+    y=KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)),
+    z=KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)),
+  )
+  q′ = (
+    x=KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)),
+    y=KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)),
+    z=KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full)),
+  )
+
+  residual = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+  Reynolds_number = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+
+  α = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+
+  θr_dτ = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+  dτ_ρ = KernelAbstractions.zeros(backend, T, size(mesh.iterators.cell.full))
+
+  x, y, z = coords(mesh)
+  spacing =
+    (minimum(diff(x; dims=1)), minimum(diff(y; dims=2)), minimum(diff(z; dims=3))) .|> T
+
+  min_x, max_x = extrema(x)
+  min_y, max_y = extrema(y)
+  min_z, max_z = extrema(z)
+
+  L = max(abs(max_x - min_x), abs(max_y - min_y), abs(max_z - min_z)) |> T
 
   if face_diffusivity === :harmonic
     mean_func = harmonic_mean # from ../averaging.jl
@@ -236,15 +311,8 @@ function update_iteration_params!(solver, ρ, Vpdτ, Δt; iter_scale=1)
 end
 
 @kernel function flux_kernel!(
-  qᵢ₊½::AbstractArray{T,2},
-  q′ᵢ₊½::AbstractArray{T,2},
-  u,
-  α,
-  θr_dτ,
-  axis,
-  index_offset,
-  mean_func::F,
-) where {T,F}
+  qᵢ₊½, q′ᵢ₊½, u, α, θr_dτ, axis, index_offset, mean_func::F
+) where {F}
   idx = @index(Global, Cartesian)
   ᵢ = idx + index_offset
 
@@ -300,8 +368,62 @@ function compute_flux!(solver::PseudoTransientSolver{2,T}, ::CurvilinearGrid2D) 
   return nothing
 end
 
+function compute_flux!(solver::PseudoTransientSolver{3,T}, ::CurvilinearGrid3D) where {T}
+  iaxis = 1
+  jaxis = 2
+  kaxis = 3
+
+  ᵢ₊½_domain = expand_lower(solver.iterators.domain.cartesian, iaxis, +1)
+  ⱼ₊½_domain = expand_lower(solver.iterators.domain.cartesian, jaxis, +1)
+  ₖ₊½_domain = expand_lower(solver.iterators.domain.cartesian, kaxis, +1)
+
+  ᵢ₊½_idx_offset = first(ᵢ₊½_domain) - oneunit(first(ᵢ₊½_domain))
+  ⱼ₊½_idx_offset = first(ⱼ₊½_domain) - oneunit(first(ⱼ₊½_domain))
+  ₖ₊½_idx_offset = first(ₖ₊½_domain) - oneunit(first(ₖ₊½_domain))
+
+  flux_kernel!(solver.backend)(
+    solver.q.x,
+    solver.q′.x,
+    solver.u,
+    solver.α,
+    solver.θr_dτ,
+    iaxis,
+    ᵢ₊½_idx_offset,
+    solver.mean;
+    ndrange=size(ᵢ₊½_domain),
+  )
+
+  flux_kernel!(solver.backend)(
+    solver.q.y,
+    solver.q′.y,
+    solver.u,
+    solver.α,
+    solver.θr_dτ,
+    jaxis,
+    ⱼ₊½_idx_offset,
+    solver.mean;
+    ndrange=size(ⱼ₊½_domain),
+  )
+
+  flux_kernel!(solver.backend)(
+    solver.q.z,
+    solver.q′.z,
+    solver.u,
+    solver.α,
+    solver.θr_dτ,
+    kaxis,
+    ₖ₊½_idx_offset,
+    solver.mean;
+    ndrange=size(ₖ₊½_domain),
+  )
+
+  KernelAbstractions.synchronize(solver.backend)
+
+  return nothing
+end
+
 @kernel function _update_kernel!(
-  u::AbstractArray{T,2},
+  u,
   u_prev,
   cell_center_metrics, # applying @Const to a struct array causes problems
   edge_metrics, # applying @Const to a struct array causes problems
@@ -311,15 +433,12 @@ end
   source_term,
   dt,
   index_offset,
-) where {T}
+)
   idx = @index(Global, Cartesian)
   idx += index_offset
 
-  qᵢ = q.x
-  qⱼ = q.y
-
   @inbounds begin
-    @inline ∇q = flux_divergence((qᵢ, qⱼ), u, α, cell_center_metrics, edge_metrics, idx)
+    @inline ∇q = flux_divergence(q, u, α, cell_center_metrics, edge_metrics, idx)
 
     u[idx] = (
       (u[idx] + dτ_ρ[idx] * (u_prev[idx] / dt - ∇q + source_term[idx])) /
@@ -404,16 +523,20 @@ function flux_divergence(
   ∂qᵢ∂ξ = (ξx^2 + ξy^2) * (qᵢ[i, j] - qᵢ[i - 1, j])
   ∂qⱼ∂η = (ηx^2 + ηy^2) * (qⱼ[i, j] - qⱼ[i, j - 1])
 
+  # ∂/∂η (α ∂u/∂ξ) aka ∂qᵢ/∂η
   ∂qᵢ∂η =
-    0.25(ηx * ξx + ηy * ξy) * (
-      (qᵢ[i, j + 1] + qᵢ[i - 1, j + 1]) - # take average on either side
-      (qᵢ[i, j - 1] + qᵢ[i - 1, j - 1])   # and do diff in j
+    0.5(ηx * ξx + ηy * ξy) * (
+      # take average and do diff in j (for ∂/∂η)
+      0.5(qᵢ[i, j + 1] + qᵢ[i - 1, j + 1]) - #
+      0.5(qᵢ[i, j - 1] + qᵢ[i - 1, j - 1])   # 
     )
 
+  # ∂/∂ξ (α ∂u/∂η)
   ∂qⱼ∂ξ =
-    0.25(ηx * ξx + ηy * ξy) * (
-      (qⱼ[i + 1, j] + qⱼ[i + 1, j - 1]) - # take average on either side
-      (qⱼ[i - 1, j] + qⱼ[i - 1, j - 1])   # and do diff in i
+    0.5(ηx * ξx + ηy * ξy) * (
+      #  take average and do diff in i (for ∂/∂ξ)
+      0.5(qⱼ[i + 1, j] + qⱼ[i + 1, j - 1]) - #
+      0.5(qⱼ[i - 1, j] + qⱼ[i - 1, j - 1])   # 
     )
 
   ∂q∂ξ_nonorth = aᵢⱼ * 0.5(qᵢ[i, j] + qᵢ[i - 1, j]) # non-orth terms
@@ -423,23 +546,241 @@ function flux_divergence(
   return ∇q
 end
 
+function flux_divergence(
+  (qᵢ, qⱼ, qₖ), u, α, cell_center_metrics, edge_metrics, idx::CartesianIndex{3}
+)
+  i, j, k = idx.I
+
+  idim, jdim, kdim = (1, 2, 3)
+  ᵢ₋₁ = shift(idx, idim, -1)
+  ⱼ₋₁ = shift(idx, jdim, -1)
+  ₖ₋₁ = shift(idx, kdim, -1)
+
+  Jᵢ₊½ = edge_metrics.i₊½.J[idx]
+  Jⱼ₊½ = edge_metrics.j₊½.J[idx]
+  Jₖ₊½ = edge_metrics.k₊½.J[idx]
+
+  Jᵢ₋½ = edge_metrics.i₊½.J[ᵢ₋₁]
+  Jⱼ₋½ = edge_metrics.j₊½.J[ⱼ₋₁]
+  Jₖ₋½ = edge_metrics.k₊½.J[ₖ₋₁]
+
+  ξx = cell_center_metrics.ξ.x₁[idx]
+  ξy = cell_center_metrics.ξ.x₂[idx]
+  ξz = cell_center_metrics.ξ.x₃[idx]
+
+  ηx = cell_center_metrics.η.x₁[idx]
+  ηy = cell_center_metrics.η.x₂[idx]
+  ηz = cell_center_metrics.η.x₃[idx]
+
+  ζx = cell_center_metrics.ζ.x₁[idx]
+  ζy = cell_center_metrics.ζ.x₂[idx]
+  ζz = cell_center_metrics.ζ.x₃[idx]
+
+  # ξxᵢ₊½ = edge_metrics.i₊½.ξ̂.x₁[idx] / Jᵢ₊½
+  # ξyᵢ₊½ = edge_metrics.i₊½.ξ̂.x₂[idx] / Jᵢ₊½
+  # ξzᵢ₊½ = edge_metrics.i₊½.ξ̂.x₃[idx] / Jᵢ₊½
+
+  # ξxᵢ₋½ = edge_metrics.i₊½.ξ̂.x₁[ᵢ₋₁] / Jᵢ₋½
+  # ξyᵢ₋½ = edge_metrics.i₊½.ξ̂.x₂[ᵢ₋₁] / Jᵢ₋½
+  # ξzᵢ₋½ = edge_metrics.i₊½.ξ̂.x₃[ᵢ₋₁] / Jᵢ₋½
+
+  # ηxᵢ₊½ = edge_metrics.i₊½.η̂.x₁[idx] / Jᵢ₊½
+  # ηyᵢ₊½ = edge_metrics.i₊½.η̂.x₂[idx] / Jᵢ₊½
+  # ηzᵢ₊½ = edge_metrics.i₊½.η̂.x₃[idx] / Jᵢ₊½
+
+  # ηxᵢ₋½ = edge_metrics.i₊½.η̂.x₁[ᵢ₋₁] / Jᵢ₋½
+  # ηyᵢ₋½ = edge_metrics.i₊½.η̂.x₂[ᵢ₋₁] / Jᵢ₋½
+  # ηzᵢ₋½ = edge_metrics.i₊½.η̂.x₃[ᵢ₋₁] / Jᵢ₋½
+
+  # ζxᵢ₊½ = edge_metrics.i₊½.ζ̂.x₁[idx] / Jᵢ₊½
+  # ζyᵢ₊½ = edge_metrics.i₊½.ζ̂.x₂[idx] / Jᵢ₊½
+  # ζzᵢ₊½ = edge_metrics.i₊½.ζ̂.x₃[idx] / Jᵢ₊½
+
+  # ζxᵢ₋½ = edge_metrics.i₊½.ζ̂.x₁[ᵢ₋₁] / Jᵢ₋½
+  # ζyᵢ₋½ = edge_metrics.i₊½.ζ̂.x₂[ᵢ₋₁] / Jᵢ₋½
+  # ζzᵢ₋½ = edge_metrics.i₊½.ζ̂.x₃[ᵢ₋₁] / Jᵢ₋½
+
+  # ξxⱼ₊½ = edge_metrics.j₊½.ξ̂.x₁[idx] / Jⱼ₊½
+  # ξyⱼ₊½ = edge_metrics.j₊½.ξ̂.x₂[idx] / Jⱼ₊½
+  # ξzⱼ₊½ = edge_metrics.j₊½.ξ̂.x₃[idx] / Jⱼ₊½
+
+  # ξxⱼ₋½ = edge_metrics.j₊½.ξ̂.x₁[ⱼ₋₁] / Jⱼ₋½
+  # ξyⱼ₋½ = edge_metrics.j₊½.ξ̂.x₂[ⱼ₋₁] / Jⱼ₋½
+  # ξzⱼ₋½ = edge_metrics.j₊½.ξ̂.x₃[ⱼ₋₁] / Jⱼ₋½
+
+  # ηxⱼ₊½ = edge_metrics.j₊½.η̂.x₁[idx] / Jⱼ₊½
+  # ηyⱼ₊½ = edge_metrics.j₊½.η̂.x₂[idx] / Jⱼ₊½
+  # ηzⱼ₊½ = edge_metrics.j₊½.η̂.x₃[idx] / Jⱼ₊½
+
+  # ηxⱼ₋½ = edge_metrics.j₊½.η̂.x₁[ⱼ₋₁] / Jⱼ₋½
+  # ηyⱼ₋½ = edge_metrics.j₊½.η̂.x₂[ⱼ₋₁] / Jⱼ₋½
+  # ηzⱼ₋½ = edge_metrics.j₊½.η̂.x₃[ⱼ₋₁] / Jⱼ₋½
+
+  # ζxⱼ₊½ = edge_metrics.j₊½.ζ̂.x₁[idx] / Jⱼ₊½
+  # ζyⱼ₊½ = edge_metrics.j₊½.ζ̂.x₂[idx] / Jⱼ₊½
+  # ζzⱼ₊½ = edge_metrics.j₊½.ζ̂.x₃[idx] / Jⱼ₊½
+
+  # ζxⱼ₋½ = edge_metrics.j₊½.ζ̂.x₁[ⱼ₋₁] / Jⱼ₋½
+  # ζyⱼ₋½ = edge_metrics.j₊½.ζ̂.x₂[ⱼ₋₁] / Jⱼ₋½
+  # ζzⱼ₋½ = edge_metrics.j₊½.ζ̂.x₃[ⱼ₋₁] / Jⱼ₋½
+
+  # ξxₖ₊½ = edge_metrics.k₊½.ξ̂.x₁[idx] / Jₖ₊½
+  # ξyₖ₊½ = edge_metrics.k₊½.ξ̂.x₂[idx] / Jₖ₊½
+  # ξzₖ₊½ = edge_metrics.k₊½.ξ̂.x₃[idx] / Jₖ₊½
+
+  # ξxₖ₋½ = edge_metrics.k₊½.ξ̂.x₁[ₖ₋₁] / Jₖ₋½
+  # ξyₖ₋½ = edge_metrics.k₊½.ξ̂.x₂[ₖ₋₁] / Jₖ₋½
+  # ξzₖ₋½ = edge_metrics.k₊½.ξ̂.x₃[ₖ₋₁] / Jₖ₋½
+
+  # ηxₖ₊½ = edge_metrics.k₊½.η̂.x₁[idx] / Jₖ₊½
+  # ηyₖ₊½ = edge_metrics.k₊½.η̂.x₂[idx] / Jₖ₊½
+  # ηzₖ₊½ = edge_metrics.k₊½.η̂.x₃[idx] / Jₖ₊½
+
+  # ηxₖ₋½ = edge_metrics.k₊½.η̂.x₁[ₖ₋₁] / Jₖ₋½
+  # ηyₖ₋½ = edge_metrics.k₊½.η̂.x₂[ₖ₋₁] / Jₖ₋½
+  # ηzₖ₋½ = edge_metrics.k₊½.η̂.x₃[ₖ₋₁] / Jₖ₋½
+
+  # ζxₖ₊½ = edge_metrics.k₊½.ζ̂.x₁[idx] / Jₖ₊½
+  # ζyₖ₊½ = edge_metrics.k₊½.ζ̂.x₂[idx] / Jₖ₊½
+  # ζzₖ₊½ = edge_metrics.k₊½.ζ̂.x₃[idx] / Jₖ₊½
+
+  # ζxₖ₋½ = edge_metrics.k₊½.ζ̂.x₁[ₖ₋₁] / Jₖ₋½
+  # ζyₖ₋½ = edge_metrics.k₊½.ζ̂.x₂[ₖ₋₁] / Jₖ₋½
+  # ζzₖ₋½ = edge_metrics.k₊½.ζ̂.x₃[ₖ₋₁] / Jₖ₋½
+
+  # flux divergence
+
+  # αᵢⱼₖ = (
+  #   ξx * (ξxᵢ₊½ - ξxᵢ₋½) +
+  #   ξy * (ξyᵢ₊½ - ξyᵢ₋½) +
+  #   ξz * (ξzᵢ₊½ - ξzᵢ₋½) +
+  #   #
+  #   ηx * (ξxⱼ₊½ - ξxⱼ₋½) +
+  #   ηy * (ξyⱼ₊½ - ξyⱼ₋½) +
+  #   ηz * (ξzⱼ₊½ - ξzⱼ₋½) +
+  #   #
+  #   ζx * (ξxₖ₊½ - ξxₖ₋½) +
+  #   ζy * (ξyₖ₊½ - ξyₖ₋½) +
+  #   ζz * (ξzₖ₊½ - ξzₖ₋½)
+  # )
+
+  # βᵢⱼₖ = (
+  #   ξx * (ηxᵢ₊½ - ηxᵢ₋½) +
+  #   ξy * (ηyᵢ₊½ - ηyᵢ₋½) +
+  #   ξz * (ηzᵢ₊½ - ηzᵢ₋½) +
+  #   #
+  #   ηx * (ηxⱼ₊½ - ηxⱼ₋½) +
+  #   ηy * (ηyⱼ₊½ - ηyⱼ₋½) +
+  #   ηz * (ηzⱼ₊½ - ηzⱼ₋½) +
+  #   #
+  #   ζx * (ηxₖ₊½ - ηxₖ₋½) +
+  #   ζy * (ηyₖ₊½ - ηyₖ₋½) +
+  #   ζz * (ηzₖ₊½ - ηzₖ₋½)
+  # )
+
+  # γᵢⱼₖ = (
+  #   ξx * (ζxᵢ₊½ - ζxᵢ₋½) +
+  #   ξy * (ζyᵢ₊½ - ζyᵢ₋½) +
+  #   ξz * (ζzᵢ₊½ - ζzᵢ₋½) +
+  #   #
+  #   ηx * (ζxⱼ₊½ - ζxⱼ₋½) +
+  #   ηy * (ζyⱼ₊½ - ζyⱼ₋½) +
+  #   ηz * (ζzⱼ₊½ - ζzⱼ₋½) +
+  #   #
+  #   ζx * (ζxₖ₊½ - ζxₖ₋½) +
+  #   ζy * (ζyₖ₊½ - ζyₖ₋½) +
+  #   ζz * (ζzₖ₊½ - ζzₖ₋½)
+  # )
+
+  ∂qᵢ∂ξ = (ξx^2 + ξy^2 + ξz^2) * (qᵢ[i, j, k] - qᵢ[i - 1, j, k])
+  ∂qⱼ∂η = (ηx^2 + ηy^2 + ηz^2) * (qⱼ[i, j, k] - qⱼ[i, j - 1, k])
+  ∂qₖ∂ζ = (ζx^2 + ζy^2 + ζz^2) * (qₖ[i, j, k] - qₖ[i, j, k - 1])
+
+  # # ---------------
+  # # ∂/∂η (α ∂u/∂ξ) aka ∂qᵢ/∂η
+  # ∂qᵢ∂η =
+  #   0.5(ηx * ξx + ηy * ξy + ηz * ξz) * (
+  #     #  take average and do diff in j (for ∂/∂η)
+  #     0.5(qᵢ[i, j + 1, k] + qᵢ[i - 1, j + 1, k]) - #
+  #     0.5(qᵢ[i, j - 1, k] + qᵢ[i - 1, j - 1, k])
+  #   )
+
+  # # ∂/∂ξ (α ∂u/∂η) aka ∂qⱼ/∂ξ
+  # ∂qⱼ∂ξ =
+  #   0.5(ηx * ξx + ηy * ξy + ηz * ξz) * (
+  #     #  take average and do diff in i (for ∂/∂ξ)
+  #     0.5(qⱼ[i, j, k] + qⱼ[i, j, k]) - #
+  #     0.5(qⱼ[i, j, k] + qⱼ[i, j, k])
+  #   )
+
+  # # ---------------
+
+  # # ∂/∂ζ (α ∂u/∂η) aka ∂qⱼ/∂ζ
+  # ∂qⱼ∂ζ =
+  #   0.5(ζx * ηx + ζy * ηy + ζy * ηy) * (
+  #     #  take average and do diff in k (for ∂/∂ζ)
+  #     0.5(qⱼ[i, j, k] + qⱼ[i, j, k]) - #
+  #     0.5(qⱼ[i, j, k] + qⱼ[i, j, k])
+  #   )
+
+  # # ∂/∂η (α ∂u/∂ζ) aka ∂qₖ/∂η
+  # ∂qₖ∂η =
+  #   0.5(ζx * ηx + ζy * ηy + ζy * ηy) * (
+  #     #  take average and do diff in j (for ∂/∂η)
+  #     0.5(qₖ[i, j, k] + qₖ[i, j, k]) - #
+  #     0.5(qₖ[i, j, k] + qₖ[i, j, k])
+  #   )
+
+  # # ---------------
+
+  # # ∂/∂ζ (α ∂u/∂ξ) aka ∂qᵢ/∂ζ
+  # ∂qᵢ∂ζ =
+  #   0.5(ζx * ξx + ζy * ξy + ζy * ξy) * (
+  #     #  take average and do diff in k (for ∂/∂ζ)
+  #     0.5(qᵢ[i, j, k] + qᵢ[i, j, k]) - #
+  #     0.5(qᵢ[i, j, k] + qᵢ[i, j, k])
+  #   )
+
+  # # ∂/∂ξ (α ∂u/∂ζ) aka ∂qₖ/∂ξ
+  # ∂qₖ∂ξ =
+  #   0.5(ζx * ξx + ζy * ξy + ζy * ξy) * (
+  #     #  take average and do diff in i (for ∂/∂ξ)
+  #     0.5(qₖ[i, j, k] + qₖ[i, j, k]) - #
+  #     0.5(qₖ[i, j, k] + qₖ[i, j, k])
+  #   )
+
+  # ---------------
+
+  # additional non-orthogonal terms
+  # ∂q∂ξ_α = αᵢⱼₖ * 0.5(qᵢ[i, j, k] + qᵢ[i - 1, j, k])
+  # ∂q∂η_β = βᵢⱼₖ * 0.5(qⱼ[i, j, k] + qⱼ[i, j - 1, k])
+  # ∂q∂ζ_γ = γᵢⱼₖ * 0.5(qₖ[i, j, k] + qₖ[i, j, k - 1])
+
+  ∇q = (
+    ∂qᵢ∂ξ + ∂qⱼ∂η + ∂qₖ∂ζ # +
+    # #
+    # ∂qᵢ∂η +
+    # ∂qᵢ∂ζ +
+    # ∂qⱼ∂ξ +
+    # ∂qⱼ∂ζ +
+    # ∂qₖ∂η +
+    # ∂qₖ∂ξ +
+    # #
+    # ∂q∂ξ_α +
+    # ∂q∂η_β +
+    # ∂q∂ζ_γ
+  )
+  return ∇q
+end
+
 @kernel function _update_resid!(
-  resid::AbstractArray{T,2},
-  cell_center_metrics,
-  edge_metrics,
-  α,
-  u,
-  u_prev,
-  (qᵢ, qⱼ),
-  source_term,
-  dt,
-  index_offset,
-) where {T}
+  resid, cell_center_metrics, edge_metrics, α, u, u_prev, q, source_term, dt, index_offset
+)
   idx = @index(Global, Cartesian)
   idx += index_offset
 
   @inbounds begin
-    @inline ∇q = flux_divergence((qᵢ, qⱼ), u, α, cell_center_metrics, edge_metrics, idx)
+    @inline ∇q = flux_divergence(q, u, α, cell_center_metrics, edge_metrics, idx)
 
     resid[idx] = -(u[idx] - u_prev[idx]) / dt - ∇q + source_term[idx]
   end
---
+++ test/integrated/3d/pt_solver/run.jl
@@ -0,0 +1,190 @@
+using CurvilinearGrids, CurvilinearDiffusion
+using Printf, Adapt
+using TimerOutputs
+using KernelAbstractions
+using Glob
+using LinearAlgebra
+
+dev = :CPU
+
+if dev === :GPU
+  @info "Using CUDA"
+  using CUDA
+  using CUDA.CUDAKernels
+  backend = CUDABackend()
+  ArrayT = CuArray
+  # CUDA.allowscalar(false)
+else
+  backend = CPU()
+  ArrayT = Array
+end
+
+# ------------------------------------------------------------
+# Grid Construction
+# ------------------------------------------------------------
+function wavy_grid(ni, nj, nhalo)
+  Lx = 12
+  Ly = 12
+  n_xy = 6
+  n_yx = 6
+
+  xmin = -Lx / 2
+  ymin = -Ly / 2
+
+  Δx0 = Lx / (ni - 1)
+  Δy0 = Ly / (nj - 1)
+
+  # Ax = 0.4 / Δx0
+  # Ay = 0.8 / Δy0
+  Ax = 0.2 / Δx0
+  Ay = 0.4 / Δy0
+
+  x = zeros(ni, nj)
+  y = zeros(ni, nj)
+  for j in 1:nj
+    for i in 1:ni
+      x[i, j] = xmin + Δx0 * ((i - 1) + Ax * sinpi((n_xy * (j - 1) * Δy0) / Ly))
+      y[i, j] = ymin + Δy0 * ((j - 1) + Ay * sinpi((n_yx * (i - 1) * Δx0) / Lx))
+    end
+  end
+
+  return CurvilinearGrid2D(x, y, nhalo)
+end
+
+function uniform_grid(nx, ny, nz, nhalo)
+  x0, x1 = (0, 1)
+  y0, y1 = (0, 1)
+  z0, z1 = (0, 1)
+
+  return CurvilinearGrids.RectlinearGrid((x0, y0, z0), (x1, y1, z1), (nx, ny, nz), nhalo)
+end
+
+function initialize_mesh()
+  ni, nj, nk = (150, 150, 150)
+  nhalo = 4
+  return uniform_grid(ni, nj, nk, nhalo)
+  # return wavy_grid(ni, nj, nk, nhalo)
+
+end
+
+# ------------------------------------------------------------
+# Initialization
+# ------------------------------------------------------------
+function init_state()
+  # mesh = adapt(ArrayT, initialize_mesh())
+  mesh = initialize_mesh()
+
+  bcs = (
+    # ilo=NeumannBC(),  #
+    # ihi=NeumannBC(),  #
+    ilo=DirichletBC(1.0),  #
+    ihi=DirichletBC(0.0),  #
+    jlo=NeumannBC(),  #
+    jhi=NeumannBC(),  #
+    klo=NeumannBC(),  #
+    khi=NeumannBC(),  #
+  )
+
+  solver = PseudoTransientSolver(mesh, bcs; backend=backend)
+
+  # Temperature and density
+  T_hot = 1e3
+  T_cold = 1e-2
+  T = ones(size(mesh.iterators.cell.full)) * T_cold
+  ρ = ones(size(mesh.iterators.cell.full))
+  source_term = ones(size(mesh.iterators.cell.full))
+  cₚ = 1.0
+
+  # Define the conductivity model
+  @inline function κ(ρ, temperature, κ0=1.0)
+    if !isfinite(temperature)
+      return 0.0
+    else
+      return κ0 * temperature^3
+    end
+  end
+
+  # fwhm = 1.0
+  # x0 = 0.0
+  # y0 = 0.0
+  # xc = Array(mesh.centroid_coordinates.x)
+  # yc = Array(mesh.centroid_coordinates.y)
+  # zc = Array(mesh.centroid_coordinates.y)
+  # for idx in mesh.iterators.cell.domain
+  #   x⃗c = centroid(mesh, idx)
+
+  #   T[idx] =
+  #     T_hot *
+  #     exp(-(((x0 - xc)^2) / fwhm + ((y0 - x⃗c.y)^2) / fwhm + ((z0 - x⃗c.z)^2) / fwhm)) +
+  #     # T_cold
+  # end
+
+  # copy!(solver.source_term, source_term) # move to gpu (if need be)
+
+  return solver, mesh, adapt(ArrayT, T), adapt(ArrayT, ρ), cₚ, κ
+end
+
+# ------------------------------------------------------------
+# Solve
+# ------------------------------------------------------------
+function run(maxiter=Inf)
+  casename = "pt_uniform_3d_no_source"
+
+  scheme, mesh, T, ρ, cₚ, κ = init_state()
+
+  global Δt = 1e-4
+  #   global Δt = 0.02
+  global t = 0.0
+  global maxt = 0.6
+
+  global iter = 0
+  global io_interval = 0.01
+  global io_next = io_interval
+
+  @timeit "save_vtk" CurvilinearDiffusion.save_vtk(scheme, T, mesh, iter, t, casename)
+
+  while true
+    if iter == 0
+      reset_timer!()
+    end
+
+    @printf "cycle: %i t: %.4e, Δt: %.3e\n" iter t Δt
+    err, subiter = CurvilinearDiffusion.PseudoTransientScheme.step!(
+      scheme, mesh, T, ρ, cₚ, κ, Δt; max_iter=1500, tol=1e-8, error_check_interval=2
+    )
+    @printf "\tL2: %.3e, %i\n" err subiter
+
+    if t + Δt > io_next
+      @timeit "save_vtk" CurvilinearDiffusion.save_vtk(scheme, T, mesh, iter, t, casename)
+      global io_next += io_interval
+    end
+
+    if t >= maxt
+      break
+    end
+
+    global iter += 1
+    global t += Δt
+    if iter >= maxiter - 1
+      break
+    end
+    # Δt = next_dt
+  end
+
+  @timeit "save_vtk" CurvilinearDiffusion.save_vtk(
+    scheme, scheme.u, mesh, iter, t, casename
+  )
+
+  print_timer()
+  return scheme, mesh, T
+end
+
+begin
+  cd(@__DIR__)
+  rm.(glob("*.vts"))
+
+  # NVTX.@range "my message" begin
+  scheme, mesh, temperature = run(10)
+  nothing
+  # end
+end
