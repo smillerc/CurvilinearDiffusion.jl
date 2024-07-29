ac93b2b4e532619bcaa7e085905802f8ca1aa465
Miller, Samuel <smil@lle.rochester.edu>
2024-07-29 16:57:18 -0400

    Pull the Reynolds number out of the solver type; can be computed on-demand

--- src/pseudo_transient/iteration_parameters.jl
+++ src/pseudo_transient/iteration_parameters.jl
@@ -7,13 +7,11 @@ function update_iteration_params!(
   L = solver.L
   α = solver.α
   β = iter_scale
-  Re = solver.Re
   dτ_ρ = solver.dτ_ρ
   θr_dτ = solver.θr_dτ
 
   @batch for idx in solver.iterators.domain.cartesian
     _Re = π + sqrt(π^2 + (L^2 * ρ[idx]) / (α[idx] * Δt))
-    Re[idx] = _Re
     dτ_ρ[idx] = (Vpdτ * L / (α[idx] * _Re)) * β
     θr_dτ[idx] = (L / (Vpdτ * _Re)) * β
   end
@@ -28,13 +26,13 @@ function update_iteration_params!(
   # β=1 / 1.2, # iteration scaling parameter
   # β = 1, # iteration scaling parameter
   #
-  @kernel function _iter_param_kernel!(Re, dτ_ρ, θr_dτ, _Vpdτ, L, _ρ, α, dt, β, I0)
+  @kernel function _iter_param_kernel!(dτ_ρ, θr_dτ, _Vpdτ, L, _ρ, α, dt, β, I0)
     idx = @index(Global, Cartesian)
     idx += I0
 
     @inbounds begin
       _Re = π + sqrt(π^2 + (L^2 * _ρ[idx]) / (α[idx] * dt))
-      Re[idx] = _Re
+      # Re[idx] = _Re
       dτ_ρ[idx] = (_Vpdτ * L / (α[idx] * _Re)) * β
       θr_dτ[idx] = (L / (_Vpdτ * _Re)) * β
     end
@@ -44,7 +42,7 @@ function update_iteration_params!(
   idx_offset = first(domain) - oneunit(first(domain))
 
   _iter_param_kernel!(solver.backend)(
-    solver.Re,
+    # solver.Re,
     solver.dτ_ρ,
     solver.θr_dτ,
     Vpdτ,
